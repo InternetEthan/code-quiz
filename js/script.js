e3bb41a88827fff7621ab164e878c2d06b5c59e8
InternetEthan <internetethan@gmail.com>
2023-08-29 18:23:27 -0500

    added displayQuestion and answerOnClick function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,47 @@
+var questionNumber = 0
+
 
 //question data
 var questions = [
-    {question: "commonly used data types do not include:", responses: ["strings","booleans","alerts","numbers"], answer: 2},
-    {question: "the condition if an if / else statement is enclosed with ______.", responses: ["quotes","curly brackets","parenthesis","square brackets"], answer: 2},
-    {question: "arrays in javascript can be used to store _______.", responses: ["numbers and strings","other arrays","booleans","all of the above"], answer: 3},
-    {question: "string values must be enclosed within ______ when being assigned to variables.", responses: ["commas","curly brackets","quotes","parenthesis"], answer: 2},
-    {question: "a very useful tool used during development and debugging for printing content to the debugger is:", responses: ["javascript","terminal/bash","for loops","console.log"], answer: 3},
+    {question: "commonly used data types do not include:", responses: ["strings","booleans","alerts","numbers"], answer: "alerts"},
+    {question: "the condition if an if / else statement is enclosed with ______.", responses: ["quotes","curly brackets","parenthesis","square brackets"], answer: "parenthesis"},
+    {question: "arrays in javascript can be used to store _______.", responses: ["numbers and strings","other arrays","booleans","all of the above"], answer: "all of the above"},
+    {question: "string values must be enclosed within ______ when being assigned to variables.", responses: ["commas","curly brackets","quotes","parenthesis"], answer: "quotes"},
+    {question: "a very useful tool used during development and debugging for printing content to the debugger is:", responses: ["javascript","terminal/bash","for loops","console.log"], answer: "console.log"},
 ]
-console.log(questions)
+
+console.log(questions);
+
+
+function displayQuestion() {
+    var main = document.querySelector("#main");
+    main.innerHTML = "";
+    var h1El = document.createElement("h1");
+    h1El.textContent = questions[questionNumber].question;
+    main.appendChild(h1El);
+
+    var answers = document.createElement("div");
+    main.appendChild(answers);
+
+    answers.addEventListener("click", answerOnClick);
+
+    for (let i = 0; i < questions[questionNumber].responses.length; i++) {
+        var button = document.createElement("button");
+        button.textContent = questions[questionNumber].responses[i];
+        answers.appendChild(button);
+    }
+}
+
+function answerOnClick(event) {
+    console.log(event.target.textContent)
+    if (event.target.textContent === questions[questionNumber].answer) {
+        console.log("correct");
+    } else {
+        //reduce time
+        console.log("incorrect");
+    }
+    questionNumber++
+    displayQuestion()
+}
+
+displayQuestion()
