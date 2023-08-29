39a1d4eca03cfc48289d720ae92a7be38a32bbe2
InternetEthan <internetethan@gmail.com>
2023-08-28 19:52:52 -0500

    added question data; including responses and answers

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ AS A coding boot camp student
 I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
 SO THAT I can gauge my progress compared to my peers
 
-##
+## acceptance criteria
 GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+//question data
+var questions = [
+    {question: "commonly used data types do not include:", responses: ["strings","booleans","alerts","numbers"], answer: 2},
+    {question: "the condition if an if / else statement is enclosed with ______.", responses: ["quotes","curly brackets","parenthesis","square brackets"], answer: 2},
+    {question: "arrays in javascript can be used to store _______.", responses: ["numbers and strings","other arrays","booleans","all of the above"], answer: 3},
+    {question: "string values must be enclosed within ______ when being assigned to variables.", responses: ["commas","curly brackets","quotes","parenthesis"], answer: 2},
+    {question: "a very useful tool used during development and debugging for printing content to the debugger is:", responses: ["javascript","terminal/bash","for loops","console.log"], answer: 3},
+]
+console.log(questions)
