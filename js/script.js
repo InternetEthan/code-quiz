a7d0c5392467f827041acffdaf4d57fa5052c0a6
InternetEthan <internetethan@gmail.com>
2023-09-01 20:22:52 -0500

    added JSON script to record and display scores; misc formating

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ startButton.addEventListener("click", displayQuestion);
 startButton.addEventListener("click", setTime);
 
 
-
 //question data
 var questions = [
     {question: "commonly used data types do not include:", responses: ["strings","booleans","alerts","numbers"], answer: "alerts"},
@@ -46,7 +45,9 @@ function answerOnClick(event) {
     if (event.target.textContent === questions[questionNumber].answer) {
         console.log("correct");
     } else {
-        //reduce time
+    //reduce time
+        secondsLeft = secondsLeft-10;
+        timeEl.textContent = secondsLeft;
         console.log("incorrect");
     }
     questionNumber++
@@ -54,25 +55,76 @@ function answerOnClick(event) {
     
 }
 
-    function allDone() {
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    if(secondsLeft <= 0  || questionNumber > 4) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+function allDone() {
     var start = document.querySelector("#start");
     start.innerHTML = "";
     var main = document.querySelector("#main");
     main.innerHTML = "";
     var h1El = document.createElement("h1");
-    h1El.textContent = "All Done!";
+    h1El.textContent = "all done!";
     main.appendChild(h1El);
     timeEl.textContent = secondsLeft;
+    
+    
+    var initialsSpan = document.createElement("span");
+    main.appendChild(initialsSpan);
+    initialsSpan.textContent = "enter your initials and click submit to record your score; select reset to try again!";
+    
+    
+    var initialsEl = document.createElement("input");
+    main.appendChild(initialsEl);
+    
+    
+    var submitBtn = document.createElement("button");
+    submitBtn.textContent = "Submit";
+    main.appendChild(submitBtn);
+
+    var resetBtn = document.createElement("button");
+    resetBtn.textContent = "Reset";
+    main.appendChild(resetBtn)
+
+    submitBtn.addEventListener("click", saveScore);
+    submitBtn.addEventListener("click", renderScores);
+    resetBtn.addEventListener("click", restart);
+
+function saveScore() {
+    var initials = initialsEl.value.trim();
+    if (!localStorage.getItem("highscores")) {
+        localStorage.setItem("highscores",JSON.stringify([{initial: initials, score: secondsLeft}]));
+    } else {
+        var highscores = JSON.parse(localStorage.getItem("highscores"));
+        highscores.push({initial: initials, score: secondsLeft});
+        localStorage.setItem("highscores", JSON.stringify(highscores))
+    }
+}
+function renderScores() {
+    var ulEl = document.createElement('ul');
+    ulEl.textContent = "current scores";
+    main.appendChild(ulEl)
+    var highscoresList = JSON.parse(localStorage.getItem("highscores"));
+    
+    for (var i = 0; i < highscoresList.length; i++) {
+        console.log(highscoresList[i]);
+
+        
+        var liEl = document.createElement('li');
+        liEl.textContent = highscoresList[i].initial + " " + highscoresList[i].score;
+        ulEl.appendChild(liEl)
+    }
+}
 }
 
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0  || questionNumber > 4) {
-      clearInterval(timerInterval);
-    }
-
-  }, 1000);
+function restart(event) {
+    event.stopPropagation();
+    location.reload();
 }
